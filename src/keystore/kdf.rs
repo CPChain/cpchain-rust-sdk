6b5fce360c8e42294cc1f47c7513bb5f7ab864c2
zgljl2012 <2693491512@qq.com>
2022-12-15 21:51:31 +0800

    scrypt sdk

--- src/keystore/kdf.rs
+++ src/keystore/kdf.rs
@@ -2,6 +2,7 @@ use std::fmt::{Display, self};
 
 use pbkdf2::{password_hash::{SaltString, PasswordHasher}, Params, Pbkdf2};
 use rand_core::OsRng;
+use scrypt::{scrypt, Params as ScryptParamsOrigin};
 use serde::{Serialize, Deserialize, de::Visitor};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -20,14 +21,17 @@ pub struct Pbkdf2Params {
 pub struct ScryptParams {
     pub dklen: usize,
     pub n: usize,
-    pub p: usize,
-    pub r: usize,
+    pub p: u32,
+    pub r: u32,
     pub salt: String,
 }
 
 #[derive(Debug, Clone)]
 pub enum KDF {
     PBKDF2(Option<Pbkdf2Params>),
+    /// Ethereum 的 Scrypt 不同于标准 Scrypt https://github.com/ethereum/go-ethereum/issues/19977
+    /// https://github.com/golang/crypto/blob/master/scrypt/scrypt.go#L200
+    /// http://ricmoo.github.io/scrypt-js/
     SCRYPT(Option<ScryptParams>),
 }
 
@@ -43,7 +47,9 @@ impl KDF {
             KDF::PBKDF2(params) => {
                 pbkdf2_derive(password, params.clone())
             },
-            KDF::SCRYPT(_) => todo!()
+            KDF::SCRYPT(params) => {
+                todo!()
+            }
         }
     }
 }
@@ -93,20 +99,24 @@ impl Display for KDF {
     }
 }
 
+fn rand_salt() -> [u8; 16] {
+    let mut salt_bytes: [u8; 16] = [0; 16];
+    let salt = SaltString::generate(&mut OsRng);
+    salt.b64_decode(&mut salt_bytes).unwrap();
+    salt_bytes
+}
+
 fn pbkdf2_derive(password: &str, params: Option<Pbkdf2Params>) -> Result<(Vec<u8>, KDF), Box<dyn std::error::Error>> {
     let kdf_params = match params {
         Some(p) => p,
         None => {
             let rounds = 262144;
             let dklen = 32;
-            let mut salt_bytes: [u8; 16] = [0; 16];
-            let salt = SaltString::generate(&mut OsRng);
-            salt.b64_decode(&mut salt_bytes).unwrap();
             Pbkdf2Params {
                 c: rounds as u64,
                 dklen: dklen,
                 prf: "hmac-sha256".to_string(),
-                salt: hex::encode(&salt_bytes),
+                salt: hex::encode(&rand_salt()),
             }
         }
     };
@@ -126,14 +136,54 @@ fn pbkdf2_derive(password: &str, params: Option<Pbkdf2Params>) -> Result<(Vec<u8
     Ok((hash.hash.unwrap().as_bytes().to_vec(), KDF::PBKDF2(Some(kdf_params))))
 }
 
+fn scrypt_derive(password: &str, params: Option<ScryptParams>) -> Result<(), Box<dyn std::error::Error>> {
+    let kdf_params = match params {
+        Some(p) => p,
+        None => {
+            ScryptParams {
+                dklen: 32,
+                // 参数 n 和 r 决定了占用的内存区域大小和哈希迭代次数
+                n: 131072,
+                p: 1,
+                r: 8,
+                salt: hex::encode(&rand_salt()),
+            }
+        },
+    };
+    let salt = SaltString::b64_encode(&hex::decode(&kdf_params.salt)?);
+    if salt.is_err() {
+        return Err(format!("Prase salt failed: {}", salt.err().unwrap()).into())
+    }
+    // let salt = salt.unwrap();
+    // Hash password to PHC string ($scrypt$...)
+    let n = (kdf_params.n as f64).log2().ceil();
+    if n >= 64.0 {
+        return Err("n is too large".into())
+    }
+    println!("-->{:?}", n);
+
+    let params = ScryptParamsOrigin::new(n as u8, kdf_params.r, kdf_params.p).unwrap();
+    let mut output: [u8; 32] = [0; 32];
+    scrypt(password.as_bytes(), &hex::decode(&kdf_params.salt)?, &params, &mut output).unwrap();
+    println!("{}\n{:?}", kdf_params.salt, hex::encode(output));
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
     use crate::keystore::kdf::pbkdf2_derive;
+
+    use super::scrypt_derive;
 
     #[test]
     fn test_pbkdf2_derive() {
         let (r, kdf) = pbkdf2_derive("123456", None).unwrap();
         println!("{}", hex::encode(&r));
         println!("{:?}", kdf);
+    }
+
+    #[test]
+    fn test_scrypt_derive() {
+        scrypt_derive("123456", None).unwrap();
     }
 }
